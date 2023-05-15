dcfb46dd88b25ae83a93fc13c52f323cf394ff63
conradov153 <conradov153@gmail.com>
2023-05-15 16:26:43 -0400

    Agregar archivos de proyecto.

---
+++ Laplaciano_Gaussiano/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Laplaciano_Gaussiano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PB_img = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filtrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laplacianoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gaussianoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laplacianoGaussianoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OFD_img = new System.Windows.Forms.OpenFileDialog();
+            this.SFD_img = new System.Windows.Forms.SaveFileDialog();
+            this.PB_result = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_img)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PB_img
+            // 
+            this.PB_img.Location = new System.Drawing.Point(12, 44);
+            this.PB_img.Name = "PB_img";
+            this.PB_img.Size = new System.Drawing.Size(300, 300);
+            this.PB_img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_img.TabIndex = 0;
+            this.PB_img.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.filtrosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(683, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // filtrosToolStripMenuItem
+            // 
+            this.filtrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.laplacianoToolStripMenuItem,
+            this.gaussianoToolStripMenuItem,
+            this.laplacianoGaussianoToolStripMenuItem});
+            this.filtrosToolStripMenuItem.Name = "filtrosToolStripMenuItem";
+            this.filtrosToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.filtrosToolStripMenuItem.Text = "filtros";
+            // 
+            // laplacianoToolStripMenuItem
+            // 
+            this.laplacianoToolStripMenuItem.Name = "laplacianoToolStripMenuItem";
+            this.laplacianoToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.laplacianoToolStripMenuItem.Text = "Laplaciano";
+            this.laplacianoToolStripMenuItem.Click += new System.EventHandler(this.laplacianoToolStripMenuItem_Click);
+            // 
+            // gaussianoToolStripMenuItem
+            // 
+            this.gaussianoToolStripMenuItem.Name = "gaussianoToolStripMenuItem";
+            this.gaussianoToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.gaussianoToolStripMenuItem.Text = "Gaussiano";
+            this.gaussianoToolStripMenuItem.Click += new System.EventHandler(this.gaussianoToolStripMenuItem_Click);
+            // 
+            // laplacianoGaussianoToolStripMenuItem
+            // 
+            this.laplacianoGaussianoToolStripMenuItem.Name = "laplacianoGaussianoToolStripMenuItem";
+            this.laplacianoGaussianoToolStripMenuItem.Size = new System.Drawing.Size(188, 22);
+            this.laplacianoGaussianoToolStripMenuItem.Text = "Laplaciano Gaussiano";
+            this.laplacianoGaussianoToolStripMenuItem.Click += new System.EventHandler(this.laplacianoGaussianoToolStripMenuItem_Click);
+            // 
+            // OFD_img
+            // 
+            this.OFD_img.FileName = "openFileDialog1";
+            // 
+            // PB_result
+            // 
+            this.PB_result.Location = new System.Drawing.Point(368, 44);
+            this.PB_result.Name = "PB_result";
+            this.PB_result.Size = new System.Drawing.Size(300, 300);
+            this.PB_result.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_result.TabIndex = 2;
+            this.PB_result.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 365);
+            this.Controls.Add(this.PB_result);
+            this.Controls.Add(this.PB_img);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_img)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB_img;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filtrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laplacianoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gaussianoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laplacianoGaussianoToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog OFD_img;
+        private System.Windows.Forms.SaveFileDialog SFD_img;
+        private System.Windows.Forms.PictureBox PB_result;
+    }
+}
+
---
+++ Laplaciano_Gaussiano/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laplaciano_Gaussiano
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap img_1;
+        bool Exist_Img = false;
+        Bitmap img_res;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SFD_img.Filter = "Images|*.png;*.bmp;*.jpg";
+        }
+
+        //Abrir 
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OFD_img.ShowDialog() == DialogResult.OK)
+            {
+                img_1 = (Bitmap)Bitmap.FromFile(OFD_img.FileName);
+                PB_img.Image = img_1;
+                Exist_Img = true;
+            }
+
+        }
+
+        //Guardar
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (SFD_img.ShowDialog() == DialogResult.OK)
+            {
+                img_res.Save(SFD_img.FileName, ImageFormat.Png);
+            }
+        }
+
+        //filtro Gaussiano
+        private void gaussianoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(Exist_Img)
+            {
+                Bitmap img_gauss = img_1;
+                //definir el tamaño de la matriz kernel
+                int tamKernel = 5;
+
+                double[,] Kernel = new double[tamKernel, tamKernel];
+
+                //definir la desviacion estandar
+                double desv = 1.4;
+
+                //llenar la matriz kernel con sus valores
+                for (int i = 0; i < tamKernel; i++)
+                {
+                    for (int j = 0; j < tamKernel; j++)
+                    {
+                        //formula para calcular los valores de la matriz kernel
+                        Kernel[i, j] = 1 / (2 * Math.PI * Math.Pow(desv, 2)) *
+                                       Math.Exp(-(Math.Pow(i - tamKernel / 2, 2) + Math.Pow(j - tamKernel / 2, 2)) /
+                                       (2 * Math.Pow(desv, 2)));
+                    }
+                }
+
+                for (int x = tamKernel / 2; x < img_1.Width - tamKernel / 2; x++)
+                {
+                    for (int y = tamKernel / 2; y < img_1.Height - tamKernel / 2; y++)
+                    {
+                        double r = 0, g = 0, b = 0;
+
+                        for (int i = 0; i < tamKernel; i++)
+                        {
+                            for (int j = 0; j < tamKernel; j++)
+                            {
+                                Color pixelColor = img_1.GetPixel(x + i - tamKernel / 2, y + j - tamKernel / 2);
+                                r += pixelColor.R * Kernel[i, j];
+                                g += pixelColor.G * Kernel[i, j];
+                                b += pixelColor.B * Kernel[i, j];
+                            }
+                        }
+
+                        // definir el nuevo valor del pixel en la imagen filtrada
+                        Color newColor = Color.FromArgb((int)r, (int)g, (int)b);
+                        img_gauss.SetPixel(x, y, newColor);
+                    }
+                }
+
+                // mostrar la imagen filtrada
+                PB_result.Image = img_gauss;
+                img_res = img_gauss;
+            }
+        }
+
+        private void laplacianoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Exist_Img)
+            {
+
+
+                // Crear una nueva imagen del mismo tamaño para almacenar el resultado
+                Bitmap resultImage = new Bitmap(img_1.Width, img_1.Height);
+
+                //Definir la matriz del filtro Laplaciano
+                int[,] laplacianFilter = new int[,]
+                {
+                { -1, -1, -1 },
+                { -1,  8, -1 },
+                { -1, -1, -1 }
+                };
+                //int[,] laplacianFilter = new int[,]
+                //{
+                //{ 0, 1, 0 },
+                //{ 1, -4, 1 },
+                //{ 0, 1, 0 }
+                //};
+                
+
+                // Aplicar el filtro a cada píxel de la imagen en escala de grises
+                for (int x = 1; x < img_1.Width - 1; x++)
+                {
+                    for (int y = 1; y < img_1.Height - 1; y++)
+                    {
+                        // Calcular el valor del nuevo píxel aplicando el filtro Laplaciano
+                        int newValue = 0;
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                Color pixelColor = img_1.GetPixel(x + i, y + j);
+                                int grayValue = pixelColor.R; // La imagen está en escala de grises, todos los canales tienen el mismo valor
+                                newValue += grayValue * laplacianFilter[i + 1, j + 1];
+                            }
+                        }
+
+                        // Asegurarse de que el valor del nuevo píxel esté en el rango [0, 255]
+                        if (newValue > 255) newValue = 255;
+                        if (newValue < 0) newValue = 0;
+
+                        newValue = 255 - newValue;
+
+
+                        // Establecer el nuevo valor del píxel en la imagen resultante
+                        Color newColor = Color.FromArgb(newValue, newValue, newValue);
+                        resultImage.SetPixel(x, y, newColor);
+                    }
+                }
+
+                // Mostrar la imagen resultante
+                PB_result.Image = resultImage;
+            }
+        }
+
+        private void laplacianoGaussianoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Exist_Img)
+            {
+                Bitmap img_gauss = new Bitmap(img_1.Width, img_1.Height);
+                int tamKernel = 5;
+                double[,] Kernel = new double[tamKernel, tamKernel];
+                double desv = 1.4;
+                for (int i = 0; i < tamKernel; i++)
+                {
+                    for (int j = 0; j < tamKernel; j++)
+                    {
+                        Kernel[i, j] = 1 / (2 * Math.PI * Math.Pow(desv, 2)) *
+                                       Math.Exp(-(Math.Pow(i - tamKernel / 2, 2) + Math.Pow(j - tamKernel / 2, 2)) /
+                                                (2 * Math.Pow(desv, 2)));
+                    }
+                }
+                for (int x = tamKernel / 2; x < img_1.Width - tamKernel / 2; x++)
+                {
+                    for (int y = tamKernel / 2; y < img_1.Height - tamKernel / 2; y++)
+                    {
+                        double r = 0, g = 0, b = 0;
+
+                        for (int i = 0; i < tamKernel; i++)
+                        {
+                            for (int j = 0; j < tamKernel; j++)
+                            {
+                                Color pixelColor = img_1.GetPixel(x + i - tamKernel / 2, y + j - tamKernel / 2);
+                                r += pixelColor.R * Kernel[i, j];
+                                g += pixelColor.G * Kernel[i, j];
+                                b += pixelColor.B * Kernel[i, j];
+                            }
+                        }
+
+                        // definir el nuevo valor del pixel en la imagen filtrada
+                        Color newColor = Color.FromArgb((int)r, (int)g, (int)b);
+                        img_gauss.SetPixel(x, y, newColor);
+                    }
+                }
+
+
+                // Crear una nueva imagen del mismo tamaño para almacenar el resultado
+                Bitmap resultImage = new Bitmap(img_gauss.Width, img_gauss.Height);
+
+                // Definir la matriz del filtro Laplaciano
+                int[,] laplacianFilter = new int[,]
+                {
+                { -1, -1, -1 },
+                { -1,  8, -1 },
+                { -1, -1, -1 }
+                };
+                //int[,] laplacianFilter = new int[,]
+                //{
+                //{ 0, 1, 0 },
+                //{ 1, -4, 1 },
+                //{ 0, 1, 0 }
+                //};
+
+                // Aplicar el filtro a cada píxel de la imagen en escala de grises
+                for (int x = 1; x < img_gauss.Width - 1; x++)
+                {
+                    for (int y = 1; y < img_gauss.Height - 1; y++)
+                    {
+                        // Calcular el valor del nuevo píxel aplicando el filtro Laplaciano
+                        int newValue = 0;
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                Color pixelColor = img_gauss.GetPixel(x + i, y + j);
+                                int grayValue = pixelColor.R; // La imagen está en escala de grises, todos los canales tienen el mismo valor
+                                newValue += grayValue * laplacianFilter[i + 1, j + 1];
+                            }
+                        }
+
+                        // Asegurarse de que el valor del nuevo píxel esté en el rango [0, 255]
+                        newValue = Math.Max(0, Math.Min(255, newValue));
+
+                        newValue = 255 - newValue;
+
+                        // Establecer el nuevo valor del píxel en la imagen resultante
+                        Color newColor = Color.FromArgb(newValue, newValue, newValue);
+                        resultImage.SetPixel(x, y, newColor);
+                    }
+                }
+
+                // Mostrar la imagen resultante
+                PB_result.Image = resultImage;
+                img_res = resultImage;
+            }
+        }
+
+        
+    }
+}
